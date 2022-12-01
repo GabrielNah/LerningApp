6c3ed26a00ae402e13240ca583b001d0dfda3255
GabrielNah <gab.086637@gmail.com>
2022-12-01 22:55:55 +0400

    added axios config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,20 @@ import LoginComponent from "./components/LoginComponent";
 import {Routes,Route,Link} from "react-router-dom";
 import RegisterComponent from "./components/RegisterComponent";
 import ForgotPassword from "./components/ForgotPassword";
-import {useEffect} from "react";
+import {useCallback, useEffect} from "react";
 import axios from "axios";
+import Profile from "./components/Profile";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-   const getCSRF =async () => {
-       await axios.get('http://localhost:8000/sanctum/csrf-cookie')
-   }
-   useEffect(()=>{
-       getCSRF();
-   },[])
   return (
     <div className="App">
       <Routes>
         <Route path='/login' element={<LoginComponent/>}/>
         <Route path='/register' element={<RegisterComponent/>}/>
         <Route path='/forgot' element={<ForgotPassword/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
     </div>
   );
--- frontend/src/Axios/AxiosInstance.js
+++ frontend/src/Axios/AxiosInstance.js
@@ -6,4 +6,49 @@ const ApiRequest = axios.create({
     },
     withCredentials:true
 });
+const onRequest = (config) => {
+    if ((
+            config.method == 'post' ||
+            config.method == 'put' ||
+            config.method == 'delete'||
+            config.method == 'patch'
+        ) &&
+        !getCookie('XSRF-TOKEN')) {
+        return setCSRFToken()
+            .then(response => config);
+    }
+    return config;
+}
+
+// A function that calls '/api/csrf-cookie' to set the CSRF cookies. The
+// default is 'sanctum/csrf-cookie' but you can configure it to be anything.
+const setCSRFToken = () => {
+    return axios.get('/sanctum/csrf-cookie');
+}
+
+ApiRequest.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    return Promise.reject(error);
+});
+ApiRequest.interceptors.request.use(onRequest, (e)=>Promise.reject(e));
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+            end = dc.length;
+        }
+    }
+    return decodeURI(dc.substring(begin + prefix.length, end));
+}
 export default ApiRequest;
