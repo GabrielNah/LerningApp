6f621710890d56f3e4055d789ac6d26cb92a5e89
GabrielNah <gab.086637@gmail.com>
2023-01-16 23:24:45 +0400

    added main layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,24 @@ import LoginComponent from "./components/LoginComponent";
 import {Routes,Route,Link} from "react-router-dom";
 import RegisterComponent from "./components/RegisterComponent";
 import ForgotPassword from "./components/ForgotPassword";
-import {useCallback, useEffect} from "react";
-import axios from "axios";
 import Profile from "./components/Profile";
 import Dashboard from "./components/Dashboard";
+import MainLayout from "./Layots/MainLayout";
+import HomeComponent from "./components/HomeComponent";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/login' element={<LoginComponent/>}/>
-        <Route path='/register' element={<RegisterComponent/>}/>
-        <Route path='/forgot' element={<ForgotPassword/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path="/" element={<MainLayout/>}>
+          <Route index element={<HomeComponent/>} />
+          <Route path='/login' element={<LoginComponent/>}/>
+          <Route path='/register' element={<RegisterComponent/>}/>
+          <Route path='/forgot' element={<ForgotPassword/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+
+        </Route>
       </Routes>
     </div>
   );
--- frontend/src/components/HeaderComponent.js
+++ frontend/src/components/HeaderComponent.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {NavLink, useLocation} from "react-router-dom";
 
 const HeaderComponent = () => {
+    const location = useLocation();
+    const [isLoginPage,setIsLoginPage]=useState(location.pathname==='/login')
+    useEffect(() => {
+        if (location.pathname==='/login'){
+            setIsLoginPage(true);
+            return;
+        }
+        setIsLoginPage(false)
+    }, [location]);
+
     return (
         <>
           <header className="d-flex flex-row align-items-center container-xl">
               <h1 className="flex-grow-1 text-dark text-center"> Some Learning Project</h1>
+              <div>
+
+                  {!isLoginPage && (<NavLink to="/login">Login</NavLink>)}
+              </div>
           </header>
         </>
     );
