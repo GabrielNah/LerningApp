590b1e69bf84e600eb98af7c43bd9643b675ec97
GabrielNah <gab.086637@gmail.com>
2023-01-17 23:37:46 +0400

    init vue in admin template

---
+++ backend/app/API/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\API;
+
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+abstract class ApiController
+{
+    public function successResponse(array $data,int $statusCode=null):JsonResponse
+    {
+       return response()->json([...[
+           'success'=>true],...$data], $statusCode??ResponseAlias::HTTP_OK);
+    }
+
+    public function errorResponse(array $data,int $statusCode):JsonResponse
+    {
+        return response()->json([...[
+            'success'=>false],...$data],$statusCode);
+    }
+
+    public function createdResponse(array $data):JsonResponse
+    {
+        return response()->json([...[
+            'success'=>true],...$data], ResponseAlias::HTTP_CREATED);
+    }
+
+    public function deletedResponse():JsonResponse
+    {
+        return response()->json([], ResponseAlias::HTTP_NO_CONTENT);
+    }
+}
---
+++ backend/resources/js/AdminTemplate/App.vue
@@ -0,0 +1,31 @@
+<template>
+<h1>my app</h1>
+    <pre>{{count}}</pre>
+    <button @click="increment">Add</button>
+    <router-view/>
+</template>
+
+<script>
+import {useStore} from "vuex"
+import {computed, ref} from "vue";
+
+export default {
+    name: "App",
+    setup(){
+        const store=useStore()
+        const getCount=store.getters.getCount
+        const increment = () => {
+          store.commit('increment')
+        }
+        const count = computed(() => {
+            return store.getters.getCount
+        })
+
+        return {count,increment}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
